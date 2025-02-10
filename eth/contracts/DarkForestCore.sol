75f7f88ee1127e7b2319d6d2c9ec086cbe81b26e
cherryblue1024 <cherryblue1024@gmail.com>
2025-02-10 15:07:46 +0800

    feat: update end time

--- eth/contracts/DarkForestCore.sol
+++ eth/contracts/DarkForestCore.sol
@@ -47,7 +47,7 @@ contract DarkForestCore is Initializable, DarkForestStorageV1 {
         VERSION = 1;
         DISABLE_ZK_CHECK = _disableZKCheck;
 
-        gameEndTimestamp = 1755439200;
+        gameEndTimestamp = 4911112800;
         target4RadiusConstant = 50;
         target5RadiusConstant = 12;
 
